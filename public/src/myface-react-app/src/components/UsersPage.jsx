ed5ca5a09b6b922e460daeb1e953321eab1b35d4
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-09-10 00:25:16 +0100

    Setting next button

--- public/src/myface-react-app/src/components/UsersPage.jsx
+++ public/src/myface-react-app/src/components/UsersPage.jsx
@@ -2,29 +2,30 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export function UsersPage() {
+  const [users, setUsers] = useState([]);
+  const [userId, setUserId] = useState([]);
 
-    const [users, setUsers] = useState([]);
-    const [userId, setUserId] = useState([])
+  const [page, setPage] = useState("/?page=1&pageSize=10")
+  const [next, setNext] = useState();
+  const [previous, setPrevious] = useState();
 
-    useEffect(() => {
-        async function fetchUsers() {
-            const response = await fetch("http://localhost:3001/users");
-            const json = await response.json();
-            setUsers(json.results);
-        }
-        fetchUsers();
-    }, [userId])
-
-    if (users.length === 0) {
-
-        return <p>There are currently no users.</p>
-
-    } else {
-
-        return (
-            users.map((user, index) => {
-                return <p><Link key={index} to={`/users/${user.id}`} onClick={() => setUserId(user.id)} value={userId}>{user.username}  </Link></p>
-            })
-        )
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await fetch(`http://localhost:3001/users/${page}`);
+      const json = await response.json();
+      setUsers(json.results);
+      setPrevious(json.previous)
+      setNext(json.next)
     }
+    fetchUsers();
+  }, [userId, page]);
+
+  if (users.length === 0) {
+    return <p>There are currently no users.</p>;
+  } else {
+    return <div>
+    {users.map((user, index) => { return <p><Link key={index} to={`/users/${user.id}`} onClick={() => setUserId(user.id)} value={userId}>{user.username}</Link></p>})} 
+    <Link>Next</Link>
+    </div>
+  } 
 }
