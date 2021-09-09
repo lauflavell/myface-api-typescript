9f6e3d8c22b2760ba7df6a2bafa21a447740101d
Lauren Flavell <lauren.flavell@softwire.com>
2021-09-09 16:25:08 +0100

    change Posts component from if to ternary

--- public/src/myface-react-app/src/components/Posts.jsx
+++ public/src/myface-react-app/src/components/Posts.jsx
@@ -3,19 +3,14 @@ import moment from "moment";
 
 export function Posts({ post, postedBy }) {
 
-    const isLikedByDislikedBy = post.likedBy !== undefined;
-    let likesDislikes;
-
-    if (isLikedByDislikedBy) {
-        likesDislikes = <p><button> 👍 </button>  {post.likedBy.length} <button> 👎 </button> {post.dislikedBy.length}</p>;
-    }
     return (
         <li className="post-item">
             <img src={post.imageUrl} alt={post.message} width="300px" />
             <p>{postedBy.username}</p>
             <p>{post.message}</p>
             <p>{moment(post.createdAt).format('DD/MM/YYYY')}</p>
-            {likesDislikes}
+            {post.likedBy !== undefined ? 
+            <p><button> 👍 </button>  {post.likedBy.length} <button> 👎 </button> {post.dislikedBy.length}</p> : <></> }
         </li>
     )
 
