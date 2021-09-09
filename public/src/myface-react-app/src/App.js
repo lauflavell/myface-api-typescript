1f2aa1808c5730f8644aea623231d19f7d0045b5
Lauren Flavell <lauren.flavell@softwire.com>
2021-09-09 11:38:59 +0100

    add BrowserRouter

--- public/src/myface-react-app/src/App.js
+++ public/src/myface-react-app/src/App.js
@@ -1,21 +1,22 @@
 import "./App.scss";
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { PostList } from "./components/PostList";
 import { UsersPage } from "./components/UsersPage";
 import { UserDetail } from "./components/UserDetail";
 
 function App() {
   return (
+    <Router>
     <main>
       <Switch>
         <Route exact path="/" component={PostList} />
         <Route path="/posts" component={PostList} />
         <Route exact path="/users" component={UsersPage} />
         <Route path="/users/58" component={UserDetail} />
-        
       </Switch>
     </main>
+    </Router>
   );
 }
 
