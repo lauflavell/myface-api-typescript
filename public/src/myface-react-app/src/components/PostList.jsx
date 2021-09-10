c21dfa36bf1f2769f77b234b6480a9a7e10f3776
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-09-10 11:41:03 +0100

    Next and Previous Buttons working on Posts page

--- public/src/myface-react-app/src/components/PostList.jsx
+++ public/src/myface-react-app/src/components/PostList.jsx
@@ -1,21 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Posts } from "./Posts";
 import { Navbar } from "./Navbar";
+import { Link } from "react-router-dom";
+
 
 export function PostList() {
 
     const [posts, setPosts] = useState([])
 
+    const [page, setPage] = useState("/posts/?page=1&pageSize=10");
+
+    let nextPage = useRef(0);
+    let prevPage = useRef(0);
+
     useEffect(() => {
         async function fetchPosts() {
-            const response = await fetch('http://localhost:3001/posts')
+            const response = await fetch("http://localhost:3001" + page)
             const json = await response.json()
             setPosts(json.results)
+            nextPage.current = json.next
+            prevPage.current = json.previous
         }
 
         fetchPosts()
 
-    },[posts])
+    },[posts, page])
 
     if (posts.length === 0) {
         return <p>There are currently no posts.</p>
@@ -23,6 +32,16 @@ export function PostList() {
         return (
             <section>
                 <Navbar />
+                <p>
+          <Link to={prevPage.current} onClick={() => setPage(prevPage.current)}>
+            Previous
+          </Link>
+        </p>
+        <p>
+          <Link to={nextPage.current} onClick={() => setPage(nextPage.current)}>
+            Next
+          </Link>
+        </p>
                 <h2>Posts</h2>
                 <ol>
                     {posts.map((post) => {
@@ -30,10 +49,22 @@ export function PostList() {
                             <Posts post={post} postedBy={post.postedBy} />)
                     })}
                 </ol>
+                <p>
+          <Link to={prevPage.current} onClick={() => setPage(prevPage.current)}>
+            Previous
+          </Link>
+        </p>
+        <p>
+          <Link to={nextPage.current} onClick={() => setPage(nextPage.current)}>
+            Next
+          </Link>
+        </p>
             </section>
         );
     }
 
 }
+
+
 
 
