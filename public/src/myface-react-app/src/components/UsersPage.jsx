29c8327814b0ae34e0f273f0f744c9a423efadaf
Erwin <erwin.valdez.16@ucl.ac.uk>
2021-09-10 11:20:20 +0100

    Next and Prev buttons working

--- public/src/myface-react-app/src/components/UsersPage.jsx
+++ public/src/myface-react-app/src/components/UsersPage.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
 export function UsersPage() {
   const [users, setUsers] = useState([]);
   const [userId, setUserId] = useState([]);
+  const [page, setPage] = useState("/users/?page=1&pageSize=10");
 
-  const [page, setPage] = useState("/?page=1&pageSize=10")
-  const [next, setNext] = useState();
-  const [previous, setPrevious] = useState();
+  let nextPage = useRef(0);
+  let prevPage = useRef(0);
 
   useEffect(() => {
     async function fetchUsers() {
-      const response = await fetch(`http://localhost:3001/users/${page}`);
+      const response = await fetch("http://localhost:3001" + page);
       const json = await response.json();
       setUsers(json.results);
-      setPrevious(json.previous)
-      setNext(json.next)
+      nextPage.current = json.next;
+      prevPage.current = json.previous;
     }
     fetchUsers();
   }, [userId, page]);
@@ -23,9 +23,33 @@ export function UsersPage() {
   if (users.length === 0) {
     return <p>There are currently no users.</p>;
   } else {
-    return <div>
-    {users.map((user, index) => { return <p><Link key={index} to={`/users/${user.id}`} onClick={() => setUserId(user.id)} value={userId}>{user.username}</Link></p>})} 
-    <Link>Next</Link>
-    </div>
-  } 
+    return (
+      <div>
+        {users.map((user, index) => {
+          return (
+            <p>
+              <Link
+                key={index}
+                to={"/users/" + user.id}
+                onClick={() => setUserId(user.id)}
+                value={userId}
+              >
+                {user.username}
+              </Link>
+            </p>
+          );
+        })}
+        <p>
+          <Link to={prevPage.current} onClick={() => setPage(prevPage.current)}>
+            Previous
+          </Link>
+        </p>
+        <p>
+          <Link to={nextPage.current} onClick={() => setPage(nextPage.current)}>
+            Next
+          </Link>
+        </p>
+      </div>
+    );
+  }
 }
